d6debcfd0d74729b79d1b71482c434d5752e284b
Matheus Turra <turra.m@northeastern.edu>
2023-04-01 19:32:33 -0400

    Output users last and first name

---
+++ p5.py
@@ -0,0 +1,7 @@
+# Acquire users first and last name. Flip the names and print input.
+# Acquire inputs from user
+f_name = input("What is your first name: ")
+l_name = input("What is your last name: ")
+
+# Output users last and first name in specific order
+print(l_name + " " + f_name)
