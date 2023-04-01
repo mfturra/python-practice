1dc194e5c54411621e586b9c34a993e3d3d2d385
Matheus Turra <turra.m@northeastern.edu>
2023-04-01 17:22:36 -0400

    p1 solution

---
+++ p1.py
@@ -0,0 +1,3 @@
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are!\n\t\tUp above the world so high,")
+print("\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are.\n")
