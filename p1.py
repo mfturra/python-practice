8a8064f85e113d63e68135cd6e552c72b5303802
Matheus Turra <turra.m@northeastern.edu>
2023-04-01 17:28:44 -0400

    Add comment explanation of code

--- p1.py
+++ p1.py
@@ -1,3 +1,4 @@
+# Print first few lines of children's song with specific format
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are!\n\t\tUp above the world so high,")
 print("\t\tLike a diamond in the sky.")
 print("Twinkle, twinkle, little star,\n\tHow I wonder what you are.\n")
