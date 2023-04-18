10c39f2bde9640ea8399b1fa986bdb19a7f0e249
Matheus Turra <turra.m@northeastern.edu>
2023-04-18 14:27:17 -0400

    Updated tip_picker mini project

--- udemy/day-4-end/tip_picker.py
+++ udemy/day-4-end/tip_picker.py
@@ -6,10 +6,15 @@ names_string = input(
 names = names_string.split(", ")
 
 # Identify the number of names provided
+names = [name.capitalize() for name in names]
 num_names = len(names)
 
 # Generate a random number from 1 to num_names
-tip_id = random.randint(1, num_names)
+tip_id = random.randint(1, num_names-1)
 
 # Print name based on random number generated
-print(f"{names[tip_id]} is going to buy the meal today!")
+print(f"{names[tip_id]} is going to buy the meal today!\n")
+
+# Alternative method
+tipper = random.choice(names)
+print(f"{tipper} is going to buy the meal today!\n")
