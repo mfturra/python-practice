2a26dd2c01c38b5dfd024b86e8c82eabd2ebc955
Matheus Turra <turra.m@northeastern.edu>
2023-04-26 10:58:57 -0400

    Updated file to make it more robust under different conditions

--- udemy/day-4-end/rock-paper-scissors.py
+++ udemy/day-4-end/rock-paper-scissors.py
@@ -31,48 +31,44 @@ scissors = '''
 
 # Gather user input
 user_input = int(input(
-    "What do you choose? Type 0 for rock, 1 for Paper, or 2 for Scissors: "))
+    "What do you choose? Type 0 for rock, 1 for Paper, or 2 for Scissors: \n"))
 
 # Print user selection
-if user_input == 0:
-    print(rock)
-elif user_input == 1:
-    print(paper)
+game_images = [rock, paper, scissors]
+
+if user_input > 2:
+    print("That's an invalid number. You lose!")
 else:
-    print(scissors)
+    print((game_images[user_input]))
 
-# Randomize computer selection
-comp_choice = random.randint(0, 2)
+    # Randomize computer selection
+    comp_choice = random.randint(0, 2)
 
-# Print algorithmic selection
-print(f"Computer chose: {comp_choice}")
+    # Print algorithmic selection
+    print(f"Computer chose: {comp_choice}")
+    print(game_images[comp_choice])
 
-if comp_choice == 0:
-    print(rock)
-elif comp_choice == 1:
-    print(paper)
-else:
-    print(scissors)
-
-# Win, loss, and tie conditions with output
-if user_input == 0:
-    if comp_choice == 0:
-        print("You tied!")
-    elif comp_choice == 1:
-        print("You lose")
-    else:
-        print("You won!")
-elif user_input == 1:
-    if comp_choice == 0:
-        print("You win!")
-    elif comp_choice == 1:
-        print("You tied!")
-    else:
-        print("You lose!")
-elif user_input == 2:
-    if comp_choice == 0:
-        print("You lose")
-    elif comp_choice == 1:
-        print("You win!")
-    else:
-        print("You tied!")
+    # Win, loss, and tie conditions with output
+    if user_input >= 3 or user_input < 0:
+        print("You typed an invalid number, you lose!")
+    elif user_input == 0:
+        if comp_choice == 0:
+            print("You tied!")
+        elif comp_choice == 1:
+            print("You lose")
+        else:
+            print("You won!")
+    elif user_input == 1:
+        if comp_choice == 0:
+            print("You win!")
+        elif comp_choice == 1:
+            print("You tied!")
+        else:
+            print("You lose!")
+    elif user_input == 2:
+        if comp_choice == 0:
+            print("You lose")
+        elif comp_choice == 1:
+            print("You win!")
+        else:
+            print("You tied!")
