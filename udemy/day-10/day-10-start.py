8e33b5c1fc975341f5511afb2f355e7d1b15dfaf
mfturra <matheus.turra.b@gmail.com>
2024-08-06 21:54:17 -0400

    Older files were updated with docstring best practices. New doc was created that checks whether input year is a leap year or not.

--- udemy/day-10/day-10-start.py
+++ udemy/day-10/day-10-start.py
@@ -1,24 +1,30 @@
-# Create a function with outputs
+# TASK
+# 1. Create a function with outputs and print the output
 
 def format_name(first_name, last_name):
-    # return statement workflow when no names are provided
+    """Modify the first and last name inputs to camel case
+    independent of case structure that was provided."""
+
+    # error handling workflow when no names are provided
     if first_name == "" or last_name == "":
         return "Valid inputs were not provided"
 
+    # format inputs to camel case
     formatted_fname = first_name.title()
     formatted_lname = last_name.title()
 
-    # return output using print
+    # produce output using print
     # print(f"{formatted_fname} {formatted_lname}")
 
-    #return output using return statement
+    # produce output using return statement
     return f"\n{formatted_fname} {formatted_lname}"
 
 # gather inputs from user
 first_name = input("\nWhat's the first name?:\n")
 last_name = input("What's the last name?:\n")
 
+# store output of function into a var
 formatted_string = format_name(first_name, last_name)
 
+# print function variable output
 print(formatted_string)
-# output = f_name
--- udemy/day-10/days-in-months.py
+++ udemy/day-10/days-in-months.py
@@ -1,37 +1,62 @@
 def is_leap(year):
+  """Receive year input and check to see whether it's a leap year or not."""
+  # check if year is divisible by 4
   if year % 4 == 0:
+    
+    # check if year is divisible by 100
     if year % 100 == 0:
+
+      # if year is divisible by 100 and 400 with no remainder then year is a leap year
       if year % 400 == 0:
+        # output should be True, producing a confirmation that leap year was found
         return True
+      
+      # if year is divisible by 100 with no remainder and not by 400 then year is not a leap year
       else:
+        # output should be False, producing a confirmation that leap year wasn't found
         return False
+
     else:
+      # output should be True, producing a confirmation that leap year was found
       return True
+
   else:
+    # output should be False, producing a confirmation that leap year wasn't found
     return False
   
-# TODO: Add more code here 👇
 def days_in_month(year, month):
-    # check if input year is a leap year
+    """Receive input of year and month. Proceed to use is_leap to check if year is a
+    leap year. If so, output the num of days in month of leap year based on list. If year 
+    input is not of a leap year, then the output of the number of days should change 
+    accordingly."""
+
+    # use is_leap function to check if input year is a leap year
     if is_leap(year):
         # num days for leap years
         leap_month_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         
-        # return day in month for leap year
+        # select month entry in respective list
         days_for_month = leap_month_days[month - 1]
+        
+        # return day in month for leap year
         return days_for_month
 
     else:
         # standard num days for non-leap years
         month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
         
-        # return day in month for non-leap year 
+        # select month entry in respective list
         days_for_month = month_days[month - 1]
+
+        # return day in month for non-leap year 
         return days_for_month
   
-  
-#🚨 Do NOT change any of the code below 
+# gather inputs for function usage
 year = int(input("\nEnter the year that you want to check: ")) # Enter a year
 month = int(input("Enter the month that you want to check: ")) # Enter a month
+
+# utilize multiple functions to check month days based on whether input is leap year or not
 days = days_in_month(year, month)
+
+# output num of days in month of leap or non-leap year
 print(days)
